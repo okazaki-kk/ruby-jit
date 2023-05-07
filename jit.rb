da6953af602ffc391ef0fe2a631cc5004ec430ac
a0171610 <kohei-okazaki@c-fo.com>
2023-05-07 12:56:22 +0900

    :heart: git => jit

--- jit.rb
+++ jit.rb
@@ -10,16 +10,16 @@ when 'init'
   path = ARGV.fetch(0, Dir.getwd)
 
   root_path = Pathname.new(File.expand_path(path))
-  git_path = root_path.join('.jit')
+  jit_path = root_path.join('.jit')
 
   %w[objects refs].each do |dir|
-    FileUtils.mkdir_p(git_path.join(dir))
+    FileUtils.mkdir_p(jit_path.join(dir))
   rescue Errno::EACCES => e
     warn "error: #{e.message}"
     exit 1
   end
 
-  puts "initialized empty jit repository in #{git_path}"
+  puts "initialized empty jit repository in #{jit_path}"
   exit 0
 else
   warn "jit: '#{command}' is not a jit command. See 'jit --help'."
